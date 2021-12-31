47234c4cab88a9d64551095afc1215457117404b
SKoprivica <strudlakoprivica@gmail.com>
2021-12-31 02:39:45 +0100

    add tabs for parking view

--- carfaactory-fe/src/components/parking/ParkingsReviw.js
+++ carfaactory-fe/src/components/parking/ParkingsReviw.js
@@ -1,9 +1,15 @@
-import { Space, Typography } from "antd";
+import { Space, Typography, Tabs } from "antd";
 import React from "react";
 import axios from "axios";
 import VehicleTable from "../vehicles/review/VehicleTable";
 
 const { Text } = Typography;
+const { TabPane } = Tabs;
+const noOcupied = { padding: 20, border: 'solid', borderColor: '#bae7ff', with: "200" }
+const ocupied = { padding: 20, border: 'solid', borderColor: '#ff7875' }
+
+const noOcupiedTab = { padding: 10, border: 'solid', borderColor: '#bae7ff', with: "200" }
+const ocupiedTab = {  padding: 10,border: 'solid', borderColor: '#ff7875' }
 
 class ParkingsReview extends React.Component {
   state = {
@@ -14,28 +20,31 @@ class ParkingsReview extends React.Component {
     axios
       .get("http://localhost:5000/api/car-factory/parking/status")
       .then((response) => {
-        console.log(response);
-        this.setState({ parkings: response.data });
+        const { data } = response;
+        data.sort((a, b) => a.occupiedArea > b.occupiedArea)
+        this.setState({ parkings: data });
       })
       .catch((error) => console.log(error));
   }
 
   render() {
+    console.log(this.state)
     return (
       <div className="page">
         <h2>Parking Review</h2>
-        <Space>{this.state.parkings.map(parking => <Parking data={parking} />)}</Space>
+        <Tabs style={{marginLeft:20}}>
+          {this.state.parkings.map(parking => <TabPane key={parking.name} tab={<div style={parking.full ? ocupiedTab : noOcupiedTab}>{parking.name}</div>}><Parking data={parking} /></TabPane>)}
+        </Tabs>
       </div>
     );
   }
 }
 
-const noOcupied = {padding: 20, border: 'solid', borderColor: '#bae7ff', with: "200" }
-const ocupied = {padding: 20,border: 'solid', borderColor: '#ff7875' }
+
 const Parking = ({ data }) => {
   return (
     <Space direction="vertical" >
-      <div direction="vertical" style={data.full ? ocupied : noOcupied} >
+      <div style={data.full ? ocupied : noOcupied} >
         <div >
           <Text strong>Name: </Text>
           <Text>{data.name}</Text>
